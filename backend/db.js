e72e3e35631987298650abd761a60fdd324a1d78
Aryan Meshram <aryanmeshram0509@gmail.com>
2026-03-19 22:18:58 +0530

    Initial commit

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "aryan0509",
+  password: "password",
   database: "attendance_management_db"
 });
 
